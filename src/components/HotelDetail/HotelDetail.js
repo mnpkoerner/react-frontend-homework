3d59636765588bba0c92a8cb35cae7b16b956ec2
mnpkoerner <mnpkoerner@gmail.com>
2021-05-09 21:49:07 -0500

    details card rendering hotel details and amenities

--- src/components/HotelDetail/HotelDetail.js
+++ src/components/HotelDetail/HotelDetail.js
@@ -1,14 +1,33 @@
 import React from 'react'
 
-export default function HotelDetail ({hotelDetails}) {
-    return(
-        <div className="hotel-details-card">
-                    <h1>{hotelDetails.hotelStaticContent.name}</h1>
-                    <div
-                            className="image"
-                            style={{ backgroundImage: `url(${hotelDetails.hotelStaticContent.mainImage.url})` }}>
-                    </div>
+export default function HotelDetail({ hotelDetails }) {
+    return (
+        <main className="hotel-details-card">
+            <header className="details-header">
+                <h1>{hotelDetails.hotelStaticContent.name}</h1>
 
-        </div>
+                <h3>{hotelDetails.hotelStaticContent.address.line1}</h3>
+
+                <h3>{hotelDetails.hotelStaticContent.address.city}, {hotelDetails.hotelStaticContent.address.stateName} {hotelDetails.hotelStaticContent.address.zipCode}</h3>
+            </header>
+
+            <main className="hotel-details-card-description">
+                <p>{hotelDetails.hotelStaticContent.description}</p>
+            </main>
+            
+            <section className="amenities">
+                <p><span className="bold-type">Ammenities include:</span>
+                    {hotelDetails.hotelStaticContent.amenities.map((amenity) => {
+                        return (
+                            <span> {amenity.name}, </span>
+                        )
+                    }
+                    )}
+                and all the comforts of home.</p>
+            </section>
+            <section className="hotel-details-pandp">
+
+            </section>
+        </main>
     )
 }
